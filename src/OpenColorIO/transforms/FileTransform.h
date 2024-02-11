72144563831ed2d3a4a63d1e17b78e784eacc8a2
sebyvillamizar <116742369+sebyvillamizar@users.noreply.github.com>
2024-02-11 10:09:41 -0500

    Update FileTransform.h
    Signed-off-by: sebyvillamizar <116742369+sebyvillamizar@users.noreply.github.com>

--- src/OpenColorIO/transforms/FileTransform.h
+++ src/OpenColorIO/transforms/FileTransform.h
@@ -137,6 +137,7 @@ public:
     int getNumFormats(int capability) const noexcept;
     const char * getFormatNameByIndex(int capability, int index) const noexcept;
     const char * getFormatExtensionByIndex(int capability, int index) const noexcept;
+    static bool IsFormatExtensionSupported(const char * ext, int capability, int index);
 private:
     FormatRegistry();
     ~FormatRegistry();
